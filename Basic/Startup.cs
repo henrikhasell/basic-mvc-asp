398e4fd6fc6023e0b8d9f3f0ef950e6242087c23
Henrik Hasell <henrikhasell@gmail.com>
2017-12-05 13:03:19 +0000

    Added bootstrap theming.

--- Basic/Startup.cs
+++ Basic/Startup.cs
@@ -22,6 +22,8 @@ namespace Basic
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
