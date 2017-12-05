74dae665f32912080c3cdc21133274c1fb2b062f
Henrik Hasell <henrikhasell@gmail.com>
2017-12-05 13:27:05 +0000

    Added tag helpers.

--- Basic/Controllers/HomeController.cs
+++ Basic/Controllers/HomeController.cs
@@ -12,5 +12,17 @@ namespace Basic.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Break()
+        {
+            return View();
+        }
     }
 }
