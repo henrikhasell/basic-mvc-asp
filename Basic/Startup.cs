389f07aa5519c3633a15fb3c72f946c33340e8bf
Henrik Hasell <henrikhasell@gmail.com>
2017-12-05 08:58:09 +0000

    Removed comments from Startup.cs

--- Basic/Startup.cs
+++ Basic/Startup.cs
@@ -11,14 +11,10 @@ namespace Basic
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -26,9 +22,6 @@ namespace Basic
                 app.UseDeveloperExceptionPage();
             }
 
-            // The default URL routing logic used by ASP.NET MVC uses a format like this to determine what code to invoke.
-            // When you run the application and don't supply any URL segments, it defaults to the "Home" controller and
-            // the "Index" action method specified in the defaults section of the code above.
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
